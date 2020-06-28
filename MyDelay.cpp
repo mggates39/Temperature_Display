0a69a7694bdba985dbdd6580deb3e4251775b358
Marshall G Gates <mggates@gmail.com>
2020-06-27 21:09:20 -0400

    Implemented repeat count
    Stop the timer after N events have occured

--- MyDelay.cpp
+++ MyDelay.cpp
@@ -6,75 +6,79 @@
 
 #include "MyDelay.h"
 
-myDelay::myDelay(void)
-{
-
-    preMills = 0;
-    setdelay(0);
-}
-myDelay::myDelay(unsigned long dtime)
-{
-    preMills = 0;
-    setdelay(dtime);
+myDelay::myDelay(void) {
+  preMills = 0;
+  setDelay(0);
 }
 
-myDelay::myDelay(unsigned long dtime, funTocall funcall)
-{
-    preMills = 0;
-    setdelay(dtime);
-    _funcall = funcall;
-    use_function = true;
+myDelay::myDelay(unsigned long dtime) {
+  preMills = 0;
+  setDelay(dtime);
 }
 
-myDelay::myDelay(unsigned long dtime, funTocall funcall, int repeatCount)
-{
-    preMills = 0;
-    setdelay(dtime);
-    _funcall = funcall;
-    use_function = true;
-    initialRepeatCount = repeatCount;
-    repeating = true;
+myDelay::myDelay(unsigned long dtime, funTocall funcall) {
+  preMills = 0;
+  setDelay(dtime);
+  _funcall = funcall;
+  useFunction = true;
 }
 
-void myDelay::setdelay(unsigned long dtime)
-{
-    delaytime = dtime;
+myDelay::myDelay(unsigned long dtime, funTocall funcall, int repeatCount) {
+  preMills = 0;
+  setDelay(dtime);
+  setRepeat(repeatCount);
+  _funcall = funcall;
+  useFunction = true;
 }
 
-void myDelay::start()
-{
-    preMills = millis();
-    running = true;
+void myDelay::setDelay(unsigned long dtime) {
+  delaytime = dtime;
 }
 
-void myDelay::stop()
-{
-    preMills = 0;
-    running = false;
+void myDelay::setRepeat(int repeatCount) {
+  initialRepeatCount = repeatCount;
+  currentRepeatCount = repeatCount;
+  repeating = true;
 }
 
-bool myDelay::update()
-{
-    if (running)
-    {
-        curMills = millis();
-        if (curMills - preMills >= delaytime)
-        {
-            preMills = curMills;
-            if (use_function == true)
-            {
-                _funcall();
-                return true;
-            }
-            else
-            {
-                return true;
-            }
+void myDelay::start() {
+  preMills = millis();
+  currentRepeatCount = initialRepeatCount;
+  running = true;
+}
+
+void myDelay::stop() {
+  preMills = 0;
+  running = false;
+}
+
+bool myDelay::update() {
+  if (running) {
+    curMills = millis();
+    if (curMills - preMills >= delaytime) {
+      if (repeating) {
+        if (initialRepeatCount != MYDELAY_REPEAT_FOREVER) {
+          currentRepeatCount--;
+          if (currentRepeatCount == 0) {
+            stop();
+          }
         }
-
-        else
-            return false;
+      }
+      preMills = curMills;
+      if (useFunction == true) {
+        _funcall();
+        return true;
+      } else {
+        return true;
+      }
+    } else  {
+      return false;
     }
-    else
-        return false;
+  } else {
+    return false;
+  }
+}
+
+bool myDelay::isRunning() {
+  return running;
 }
--- MyDelay.h
+++ MyDelay.h
@@ -1,28 +1,30 @@
-
 #ifndef _MyDelay_h_
 #define _MyDelay_h_
 #define MYDELAY_REPEAT_FOREVER -1
+
 typedef void (*funTocall)(void);
-class myDelay{
-	public:
-	myDelay(void);
-	myDelay(unsigned long dtime);
-	myDelay(unsigned long dtime, funTocall funcall);
+
+class myDelay {
+  public:
+    myDelay(void);
+    myDelay(unsigned long dtime);
+    myDelay(unsigned long dtime, funTocall funcall);
     myDelay(unsigned long dtime, funTocall funcall, int repeatCount);
-	
-	
-	void setdelay(unsigned long);
-	bool update();
-	bool isRunnint();
-	void start();
+
+
+    void setDelay(unsigned long);
+    void setRepeat(int);
+    bool update();
+    bool isRunning();
+    void start();
     void stop();
-	
-	private:
-	unsigned long preMills, curMills, delaytime;
+
+  private:
+    unsigned long preMills, curMills, delaytime;
     int initialRepeatCount = MYDELAY_REPEAT_FOREVER;
-    int repeatCount;
-	funTocall _funcall;
-	bool use_function = false;
+    int currentRepeatCount = 0;
+    funTocall _funcall;
+    bool useFunction = false;
     bool running = false;
     bool repeating = true;
 };
